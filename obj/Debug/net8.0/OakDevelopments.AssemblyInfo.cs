9d3e09bcb5514bc4ee2860d3f1958b303d5af1dd
Lindiwe Mtshengu <lindiwemtshengumonde@gmail.com>
2026-06-18 18:19:07 +0200

    Fixing mobile view

--- obj/Debug/net8.0/OakDevelopments.AssemblyInfo.cs
+++ obj/Debug/net8.0/OakDevelopments.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OakDevelopments")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec7101631f5adfd31db3c7741e79666772dcbd51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76b33239374bb018194887a588b81e93eeef22e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("OakDevelopments")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OakDevelopments")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
